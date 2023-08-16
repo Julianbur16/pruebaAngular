09a4efb7b316cb9bb8599529068195f5ebf9f22a
Alejandro Burbano <Julianbur16@gmail.com>
2023-08-16 03:38:45 -0500

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { BodyComponent } from './body/body.component';
-import { BodyDrawMechanicsComponent } from './body-draw-mechanics/body-draw-mechanics.component';
-import { BodyProgramationComponent } from './body-programation/body-programation.component';
-import { BodyAutomationComponent } from './body-automation/body-automation.component';
+
 
 const routes: Routes =[
-  {path: '', redirectTo: 'Perfil',pathMatch: 'full'},
-  {path: 'Perfil',component: BodyComponent },
-  {path: 'Dibujo_mecanico',component: BodyDrawMechanicsComponent},
-  {path: 'Programmation',component: BodyProgramationComponent},
+
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
+
 
 import { MatButtonModule } from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
@@ -14,21 +12,14 @@ import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { BodyDrawMechanicsComponent } from './body-draw-mechanics/body-draw-mechanics.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BodyProgramationComponent } from './body-programation/body-programation.component';
-import { BodyAutomationComponent } from './body-automation/body-automation.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    BodyComponent,
-    FooterComponent,
-    BodyDrawMechanicsComponent,
-    BodyProgramationComponent,
-    BodyAutomationComponent,
+    AppComponent
    
   ],
   imports: [
